fdafee150007bdcd72d3c5f369e6f9385e3b994d
Codart-7 <iconmoa@gmail.com>
2024-01-29 16:55:39 +0100

    go vars

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var mike string = "sabi boy, you go explain tire,"
+	var test, scoot float64
+
+	test, scoot = 36, 25
+
+	fmt.Println(mike, "no evidence!")
+	fmt.Println(math.Floor(test))
+	fmt.Println(math.Floor(scoot))
 }
